3792233e7dc8e0a63928e76a7755dc1ebedcbd53
Hwowon <s2018s06@e-mirim.hs.kr>
2020-08-25 14:23:41 +0900

    frist commit

---
+++ app/src/main/java/com/android/basicchat/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.android.basicchat
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_login.login_button_main
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private val TAG =MainActivity::class.java.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        auth = FirebaseAuth.getInstance()
+
+        join_button.setOnClickListener {
+
+            val email =email_area.text.toString()
+            val password = password_area.text.toString()
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+
+                        Log.d(TAG,"성공")
+
+                    } else {
+                        Log.d(TAG,"실패")
+                    }
+
+                    // ...
+                }
+        }
+
+        login_button_main.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
